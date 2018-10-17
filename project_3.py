849b5c7bc21e24e99c35ee10b266359b747e7a12
FISHER-WHITE <43434262+FISHER-WHITE@users.noreply.github.com>
2018-10-17 10:40:25 -0500

    An outline for the project; some code (read below)
    Use a program like WingIDE to open and edit the code, and IDLE shell to run. Version 3.6 was used.

---
+++ project_3.py
@@ -0,0 +1,69 @@
+# File:        project3.py
+# Authors:     Fisher White, Collin Marshall, Layne Moore
+# Date:        10/17/18
+# Class:       TCMG 476 500
+# Description: Project 3
+
+
+# Define any variables/components other than main
+
+
+# Define main
+def main():
+
+    # Check to see if raw data is already saved to local storage
+
+
+    # If not, download and save the raw data from website
+    # https://s3.amazonaws.com/tcmg476/http_access_log
+
+    # Parse (Read/Apply Filters to) the raw data
+    
+    
+        # Open the local file
+    
+    
+        # Find the number of total requests made
+        
+    
+        # Find the number of requests made each day
+    
+    
+        # Find the percentage of unsuccessful requests
+    
+    
+        # Find the percentage of redirected requests
+    
+    
+        # Find the most requested file
+    
+    
+        # Find the least requested file
+    
+        
+        # Print the number of total requests made to screen
+        print ("There were " , , " total requests.")
+        
+        # Print the number of requests made each day to screen
+        print ("")        
+        
+        # Print the percentage of unsuccessful requests to screen
+        print ("There were " , , " unsuccessful requests.")
+        
+        # Print the percentage of redirected requests to screen
+        print ("There were " , , " redirected requests.")
+        
+        # Print the most requested file to screen
+        print ("The most requested file was: " , )
+        
+        # Print the least requested file to screen
+        print ("The least requested file was: ")        
+        
+        # Write data to new local files (including files for each month)
+
+
+        # Close the open local files
+
+
+# Call main
+main()
